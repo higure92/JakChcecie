1a24689c1d7522cb0de397cca761eb2f512ff194
higure92 <deregowski.k@gmail.com>
2018-10-10 17:31:38 +0200

    Initial commit

---
+++ src/main/java/pl/infoshare/App.java
@@ -0,0 +1,18 @@
+package pl.infoshare;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        String age = "26";
+        String city = "Olsztyn";
+        String high = "180";
+        System.out.print(age + "\n" + city + " " + high);
+    };
+    };
+
