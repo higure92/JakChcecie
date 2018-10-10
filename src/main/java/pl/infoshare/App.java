4a323d724215e0b5e8ec07dbe64b2a5eec7b9f11
higure92 <deregowski.k@gmail.com>
2018-10-10 20:05:48 +0200

    Zajecia 10.10.2018

--- src/main/java/pl/infoshare/App.java
+++ src/main/java/pl/infoshare/App.java
@@ -1,18 +1,54 @@
 package pl.infoshare;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
 
-        String age = "26";
-        String city = "Olsztyn";
-        String high = "180";
-        System.out.print(age + "\n" + city + " " + high);
-    };
-    };
+        double a = 90.99;
+        System.out.println(a);
 
+        int b = (int) a;
+        System.out.println(b);
+        char c = 'a';
+        System.out.println(c);
+        char d = (char) 90;
+        System.out.println(d);
+        System.out.println("Wynik to: " + (b / a + d));
+        byte age = 26;
+        if (age >= 20 && age <= 30) {
+            System.out.println("Wiek jest wiekszy rowny 20 lat i mniejszy rowny 30");
+        }
+        if (age == 26 || age == 28) {
+            System.out.println("Wiek jest rowny 26 lat lub 28");
+        }
+
+        int agePrim = 20;
+        switch(agePrim) {
+
+
+
+
+            case 20: {
+                System.out.println("Wiek jest rowny 20");
+                break;
+            }
+            case 30: {
+                System.out.println("Wiek jest rwny 30");
+                break;
+
+            }
+            default: {
+                System.out.println("Po prostu jestes stary.");
+            }
+        }
+
+        Scanner scanner = new Scanner(System.in);
+        
+    }
+
+
+    }
