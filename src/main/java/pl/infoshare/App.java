a53fcc40cbc1fa0f7ba15c9b34dc812169b894b0
higure92 <deregowski.k@gmail.com>
2018-10-15 20:12:43 +0200

    co

--- src/main/java/pl/infoshare/App.java
+++ src/main/java/pl/infoshare/App.java
@@ -1,5 +1,6 @@
 package pl.infoshare;
 
+import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -27,9 +28,7 @@ public class App {
         }
 
         int agePrim = 20;
-        switch(agePrim) {
-
-
+        switch (agePrim) {
 
 
             case 20: {
@@ -47,8 +46,40 @@ public class App {
         }
 
         Scanner scanner = new Scanner(System.in);
-        
-    }
 
 
-    }
+        System.out.println("1 2 3 4 5 6 7 8 9 10");
+        int[] integerArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+
+
+
+        }
+
+        int winningNumber = new Random().nextInt(10 ) + 1;
+
+        Scanner liczba = new Scanner (System.in );
+        while (true) {
+        System.out.println("Podaj liczbe od 1 do 10");
+        int liczba2 = Scanner.nextInt();
+        if (liczba2 <1 || liczba2 >10) {
+            System.out.println("Podaj liczbe z innego przedzialu");
+        }
+            if (liczba2= winningNumber) {
+                System.out.println("WYGRALES");
+                break;
+            }
+            else System.out.println("GRAJ DALEJ");
+        }
+        }
+
+
+
+
+
+
+
+
+
+
+
